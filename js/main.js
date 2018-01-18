4a0c6c346efde50677d888829d1aa59271969ee2
tabudongwodelei <617435300@qq.com>
2018-01-18 18:00:27 +0800

    7

--- js/main.js
+++ js/main.js
@@ -2,34 +2,34 @@
  * Created by lenovo on 18/1/5 005.
  */
 $(function () {
-    $(".mainer3_three .pinpai").click(function(){
-        $(".mainer3_three .pinpai").addClass("active");
-        $(".mainer3_three .qiye").removeClass("active");
-        $(".mainer3_three .dinghuo").removeClass("active");
-        $(".mainer3_three .shop").removeClass("active");
+    $(".mainer6_three .pinpai").click(function(){
+        $(".mainer6_three .pinpai").addClass("active");
+        $(".mainer6_three .qiye").removeClass("active");
+        $(".mainer6_three .dinghuo").removeClass("active");
+        $(".mainer6_three .shop").removeClass("active");
     })
 });
 $(function () {
-    $(".mainer3_three a.qiye").click(function(){
-        $(".mainer3_three a.qiye").addClass("active");
-        $(".mainer3_three a.pinpai").removeClass("active");
-        $(".mainer3_three a.dinghuo").removeClass("active");
-        $(".mainer3_three a.shop").removeClass("active");
+    $(".mainer6_three a.qiye").click(function(){
+        $(".mainer6_three a.qiye").addClass("active");
+        $(".mainer6_three a.pinpai").removeClass("active");
+        $(".mainer6_three a.dinghuo").removeClass("active");
+        $(".mainer6_three a.shop").removeClass("active");
     })
 });
 $(function () {
-    $(".mainer3_three a.dinghuo").click(function(){
-        $(".mainer3_three a.dinghuo").addClass("active");
-        $(".mainer3_three a.pinpai").removeClass("active");
-        $(".mainer3_three a.qiye").removeClass("active");
-        $(".mainer3_three a.shop").removeClass("active");
+    $(".mainer6_three a.dinghuo").click(function(){
+        $(".mainer6_three a.dinghuo").addClass("active");
+        $(".mainer6_three a.pinpai").removeClass("active");
+        $(".mainer6_three a.qiye").removeClass("active");
+        $(".mainer6_three a.shop").removeClass("active");
     })
 });
 $(function () {
-    $(".mainer3_three a.shop").click(function(){
-        $(".mainer3_three a.shop").addClass("active");
-        $(".mainer3_three a.pinpai").removeClass("active");
-        $(".mainer3_three a.qiye").removeClass("active");
-        $(".mainer3_three a.dinghuo").removeClass("active");
+    $(".mainer6_three a.shop").click(function(){
+        $(".mainer6_three a.shop").addClass("active");
+        $(".mainer6_three a.pinpai").removeClass("active");
+        $(".mainer6_three a.qiye").removeClass("active");
+        $(".mainer6_three a.dinghuo").removeClass("active");
     })
 });
